ca432c1b5eff912afe2fa8756bda51abf9cb72cc
charrde <charrrde@gmail.com>
2024-09-07 15:37:21 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,109 @@
+require('dotenv').config();
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { createHandler } = require('graphql-http/lib/use/express');
+const axios = require('axios');
+
+const PODCHASER_API_KEY = process.env.PODCHASER_API_KEY;
+const PODCHASER_API_URL = process.env.PODCHASER_API_URL;
+
+// Define your GraphQL schema with more complex queries for sorting, pagination, etc.
+const schema = buildSchema(`
+    type Query {
+        searchPodcasts(query: String!): [Podcast]
+    }
+
+	type Podcast {
+		title: String
+		description: String
+	}
+`);
+
+// Resolver functions
+const root = {
+	searchPodcasts: async ({ query }) => {
+		try {
+			const response = await axios.post(
+				PODCHASER_API_URL,
+				{
+					query: `
+						query searchPodcasts($query: String!) {
+							podcasts(searchTerm: $query) {
+								data {
+									title
+									description
+								}
+							}
+						}
+					`,
+					variables: { query },
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${PODCHASER_API_KEY}`,
+					},
+				}
+			);
+
+			const podcasts = response.data.data.podcasts.data;
+			if (!podcasts) {
+				throw new Error('Podcasts field is undefined in the API response');
+			}
+			return podcasts;
+
+		} catch (error) {
+			throw new Error('Failed to fetch podcasts from Podchaser API');
+		}
+	},
+};
+
+const app = express();
+
+app.get('/graphiql', (req, res) => {
+	res.send(`
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>GraphiQL</title>
+			<link href="https://unpkg.com/graphiql/graphiql.min.css" rel="stylesheet" />
+		</head>
+		<body style="margin: 0;">
+			<div id="graphiql" style="height: 100vh;"></div>
+
+			<script crossorigin src="https://unpkg.com/react/umd/react.production.min.js"></script>
+			<script crossorigin src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
+			<script crossorigin src="https://unpkg.com/graphiql/graphiql.min.js"></script>
+
+			<script>
+				const graphQLFetcher = graphQLParams =>
+					fetch('/graphql', {
+						method: 'post',
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						body: JSON.stringify(graphQLParams),
+					}).then(response => response.json());
+
+				ReactDOM.render(
+					React.createElement(GraphiQL, { fetcher: graphQLFetcher }),
+					document.getElementById('graphiql'),
+				);
+			</script>
+		</body>
+		</html>
+	`);
+});
+
+app.use(
+	'/graphql',
+	createHandler({
+		schema: schema,
+		rootValue: root,
+		pretty: true,
+	})
+);
+
+app.listen(4000, () => {
+	console.log('Server running on http://localhost:4000/graphql');
+	console.log('GraphiQL available at http://localhost:4000/graphiql');
+});
